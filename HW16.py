b0b0b4c09d0a6b4c900cb20769ed74babf8004b3
Aviralrathi314 <rathiaviral@gmail.com>
2025-07-31 14:12:37 +0530

    Add files via upload

---
+++ HW16.py
@@ -0,0 +1,7 @@
+switchoff = input("Do you want the system to switch off?:")
+if switchoff == "yes":
+    print("Okay, switching of the system in 1 minute")
+elif switchoff == "no":
+    print("Abort system switchoff")
+else:
+    print("invalid input, please try again")
